167aefc8e529f66d879835edd570491ba8e4c35b
Alec Wells <alec.wells47@gmail.com>
2020-02-19 11:31:08 -0700

    Add node class

---
+++ linked_lists/ruby/node.rb
@@ -0,0 +1,13 @@
+class Node
+  attr_reader :data, :next_node
+
+  def initialize(data, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
+
+  def tail?
+    next_node.nil?
+  end
+
+end
